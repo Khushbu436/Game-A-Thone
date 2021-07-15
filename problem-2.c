11d6b907c94f5a0e420e279c818f6bc23b9b4d93
Khushbu436 <76000451+Khushbu436@users.noreply.github.com>
2021-07-15 22:12:41 +0530

    Create problem-2.c

---
+++ problem-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    char name[20];
+
+    printf("Enter Your Name : ");
+    scanf("%s",name);
+
+    printf("Welcome to YCCE  : %s\n",name);
+
+    return 0;
+}
